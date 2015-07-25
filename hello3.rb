1312064aa4ce7261fb2fafba25dc5d919e34df70
Margeaux Spring <contact@cheapwebmonkey.com>
2015-07-25 09:20:38 -0400

    another super simple ruby hello

---
+++ hello3.rb
@@ -0,0 +1,8 @@
+name = "Margeaux"
+string = "Hey.\n\tMy name is #{name}.\n\tWhaddup playa?"
+puts string
+
+year =2015
+future=5
+puts "The year is #{year}"
+puts "In #{future} years the year will be #{year+future}"
