fc848aa7997d13011b77ae69b2869ae5384c3a57
Margeaux Spring <contact@cheapwebmonkey.com>
2015-07-25 15:10:59 -0400

    simple rubmy methods demo

---
+++ methods.rb
@@ -0,0 +1,12 @@
+#print out that we are adding 2 numbers together
+
+def add(a, b) 
+  puts "Adding #{a} and #{b}:"
+  
+  #return the sum of the 2 numbers
+  return a+b
+end
+
+puts add(2, 3)
+puts add(4, 5)
+puts add(10, 12)
